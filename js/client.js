68fea9d768486847ce5a3a2452908b54c17ae999
KRISHNA CHAITANYA <59798012+Krishduttm@users.noreply.github.com>
2021-05-17 15:13:16 +0530

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,65 @@
+const socket = io('http://localhost:8000');
+
+// Get DOM elements in respective Js variables
+const form = document.getElementById('reply');
+const men = document.getElementById('watch');
+const messageInput = document.getElementById('msginp')
+const messageContainer = document.querySelector(".container")
+const messageContainer2 = document.querySelector(".container2")
+
+// Audio that will play on receiving messages
+var audio = new Audio('ting.mp3');
+
+// Function which will append event info to the contaner
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position =='left'){ 
+        audio.play();
+    }
+}
+
+const active = (data)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = data;
+    messageElement.classList.add('message');
+    messageContainer2.active(messageElement);
+
+}
+
+
+// Ask new user for his/her name and let the server know
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+// If a new user joins, receive his/her name from the server
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'right')
+})
+
+// If server sends a message, receive it
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+
+})
+
+// If a user leaves the chat, append the info to the container
+socket.on('left', name =>{
+    append(`${name} left the chat`, 'right')
+})
+
+// If the form gets submitted, send server the message
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
+men.addEventListener('menu', (e) => {
+    e.preventDefault();
+    
+})
